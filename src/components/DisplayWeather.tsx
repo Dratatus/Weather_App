7e31d376613d53009fba5d81afc35d1b676e3514
Mateusz Stradomski <mateusz.stradomski01@gmail.com>
2024-01-16 20:04:58 +0100

    classes

---
+++ src/components/DisplayWeather.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { MainWrapper } from './styles.module'
+import { AiOutlineSearch } from "react-icons/ai"
+import { WiHumidity } from "react-icons/wi"
+import { SiWindicss } from "react-icons/si"
+
+const DisplayWeather = () => {
+    return (
+        <MainWrapper>
+            <div className="container">
+                <div className="SearchArea">
+                    <input type='text' placeholder='enter a city'></input>
+
+                    <div className="SearchCircle">
+                        <AiOutlineSearch className='searchIcon'></AiOutlineSearch>
+                    </div>
+                </div>
+
+                <div className="weatherArea">
+                    <h1>Auckland</h1>
+                    <span>Nz</span>
+                    <div className="icon">
+                        icon
+                    </div>
+                    <h1>18c</h1>
+                    <h2>cloudy</h2>
+                </div>
+
+                <div className="bottomInfoArea">
+                    <div className="humidityLevel">
+                        <WiHumidity className="windIcon"></WiHumidity>
+                        <div className="humidInfo">
+                            <h1>60%</h1>
+                            <p>Humidity</p>
+                        </div>
+                    </div>
+
+
+                    <div className="wind">
+                        <SiWindicss className='windIcon'></SiWindicss>
+                        <div className="humidInfo">
+                            <h1>2.35km/h</h1>
+                            <p>wind speed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </MainWrapper>
+    )
+}
+
+export default DisplayWeather
