20d747b4a030d141ddbbb0714c84b3f52a375c0e
Daniel <daniel.partatus@onet.pl>
2024-01-24 18:27:25 +0100

    fetching from api works, added custom css

--- src/components/DisplayWeather.tsx
+++ src/components/DisplayWeather.tsx
@@ -1,78 +1,82 @@
-import React from 'react'
-import { MainWrapper } from './styles.module'
-import { AiOutlineSearch } from "react-icons/ai"
-import { WiHumidity } from "react-icons/wi"
-import { SiLatex, SiWindicss } from "react-icons/si"
-import { BsFillSunFill, BsCloudyFill, BsFillCloudRainFill, BsCloudFog2Fill } from "react-icons/bs"
-import { RiLoaderFill } from "react-icons/ri"
-import { TiWeatherPartlySunny } from "react-icons/ti"
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { AiOutlineSearch } from "react-icons/ai";
+import "./displayWeather.css"
 
+interface WeatherDataProps {
+    city: string;
+    desc: string;
+    temp: number;
+    image: string;
+    wind: number;
+    pressure: number;
+}
 
-const DisplayWeather = () => {
-
-    const api_key = "xxx";
+const DisplayWeather: React.FC = () => {
+    const api_key = "fc60a7af15d3d4eaf7941db38bd2da55";
     const api_Endpoint = "https://api.openweathermap.org";
+    const [weatherData, setWeatherData] = useState<WeatherDataProps[]>([]);
+    const [inputValue, setInputValue] = useState('');
 
-    const fetchCurrentWeather = async (lat:number, lon:number) => {
-        const url = `${api_Endpoint}weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
-        const response = await axios.get(url);
-        return response.data;
-    }
+    useEffect(() => {
+        const storedData = localStorage.getItem('weatherApi');
+        if (storedData) {
+            setWeatherData(JSON.parse(storedData));
+        }
+    }, []);
 
-    React.useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const {latitude, longitude} = position.coords;
-            Promise.all([fetchCurrentWeather(latitude, longitude)]).then(
-                ([currentWeather]) => {
-                    console.log(currentWeather)
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get(`${api_Endpoint}/data/2.5/weather`, {
+                params: {
+                    q: inputValue,
+                    units: 'metric',
+                    appid: api_key
                 }
-            )
-        })
-    })
+            });
+            const data = response.data;
+            const newWeather: WeatherDataProps = {
+                city: data.name,
+                desc: data.weather[0].description,
+                temp: Math.round(data.main.temp),
+                image: data.weather[0].icon,
+                wind: data.wind.speed,
+                pressure: data.main.pressure,
+            };
+            setWeatherData(prev => [...prev, newWeather]);
+            localStorage.setItem('weatherApi', JSON.stringify([...weatherData, newWeather]));
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    const renderWeatherData = (data: WeatherDataProps, index: number) => (
+        <div key={index} className="weatherArea">
+            <h1>{data.city}</h1>
+            <h1>{data.temp}°C</h1>
+            <h2>{data.desc}</h2>
+            {data.image && (
+                <img src={`http://openweathermap.org/img/wn/${data.image}.png`} alt="Weather icon" />
+            )}
+        </div>
+    );
 
     return (
-        <MainWrapper>
             <div className="container">
                 <div className="SearchArea">
-                    <input type='text' placeholder='enter a city'></input>
-
-                    <div className="SearchCircle">
+                    <input type='text' placeholder='Enter a city' value={inputValue} onChange={handleInputChange}></input>
+                    <div className="SearchCircle" onClick={fetchWeatherData}>
                         <AiOutlineSearch className='searchIcon'></AiOutlineSearch>
                     </div>
                 </div>
 
-                <div className="weatherArea">
-                    <h1>Auckland</h1>
-                    <span>Nz</span>
-                    <div className="icon">
-                        icon
-                    </div>
-                    <h1>18c</h1>
-                    <h2>cloudy</h2>
-                </div>
-
-                <div className="bottomInfoArea">
-                    <div className="humidityLevel">
-                        <WiHumidity className="windIcon"></WiHumidity>
-                        <div className="humidInfo">
-                            <h1>60%</h1>
-                            <p>Humidity</p>
-                        </div>
-                    </div>
-
-
-                    <div className="wind">
-                        <SiWindicss className='windIcon'></SiWindicss>
-                        <div className="humidInfo">
-                            <h1>2.35km/h</h1>
-                            <p>wind speed</p>
-                        </div>
-                    </div>
-                </div>
+                {weatherData.map(renderWeatherData)}
             </div>
-        </MainWrapper>
-    )
-}
+    );
+};
 
-export default DisplayWeather
+export default DisplayWeather;
